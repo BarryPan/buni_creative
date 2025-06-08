e5fcadb19f750afdafdefd48d9ebaf44cc7303c4
Panda <panda061325@gmail.com>
2025-06-07 23:35:15 -0400

    change className

--- app/page.tsx
+++ app/page.tsx
@@ -7,17 +7,17 @@ export default function Home() {
   return (
     <MainLayout>
       {/* Hero 對應 #home 或 # */}
-      <div class="border-b" id="home">
+      <div className="border-b" id="home">
         <HeroSection />
       </div>
 
       {/* Service 對應 #service */}
-      <div class="px-4 lg:px-16 border-b" id="service">
+      <div className="px-4 lg:px-16 border-b" id="service">
         <ServiceSection />
       </div>
 
       {/* Gallery 對應 #gallery */}
-      <div class="px-4 lg:px-16" id="gallery">
+      <div className="px-4 lg:px-16" id="gallery">
         <GallerySection />
       </div>
     </MainLayout>
